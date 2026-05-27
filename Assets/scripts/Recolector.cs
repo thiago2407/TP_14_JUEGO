80ef7365959e9c5203822f1a800b25ace9b7857e
thiago2407 <titibin24@gmail.com>
2026-05-27 13:43:21 -0300

    1

---
+++ Assets/scripts/Recolector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Recolector : MonoBehaviour
+{
+    int contador = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coleccionable"))
+        {
+            contador++;
+
+            Debug.Log("Objetos recolectados: " + contador);
+
+            Destroy(other.gameObject);
+        }
+    }
+}
