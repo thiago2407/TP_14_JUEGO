216e502b486029d8abd58bff875ad408a631f8a7
thiago2407 <titibin24@gmail.com>
2026-05-29 08:26:28 -0300

    1

---
+++ Assets/scripts/Temporizador.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Temporizador : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timer = 60f;
+
+    private void Start()
+    {
+        MostrarTiempo();
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+            {
+                timer = 0;
+            }
+
+            MostrarTiempo();
+        }
+    }
+
+    private void MostrarTiempo()
+    {
+        int segundos = Mathf.CeilToInt(timer);
+        timerText.text = "00:" + segundos.ToString("00");
+    }
+}
