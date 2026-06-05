7c30b97839b5c2c9e1fc59527b2399b6f5ef016b
thiago2407 <titibin24@gmail.com>
2026-06-05 08:04:41 -0300

    rfef

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject panelWin;
+    public GameObject panelGameOver;
+
+    public int puntos = 0;
+    public int puntosParaGanar = 6;
+
+    public float tiempoLimite = 60f;
+    private float tiempoActual;
+
+    private bool juegoTerminado = false;
+
+    void Start()
+    {
+        tiempoActual = tiempoLimite;
+
+        panelWin.SetActive(false);
+        panelGameOver.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (juegoTerminado == false)
+        {
+            tiempoActual -= Time.deltaTime;
+
+            if (puntos >= puntosParaGanar)
+            {
+                Ganar();
+            }
+
+            if (tiempoActual <= 0 && puntos < puntosParaGanar)
+            {
+                Perder();
+            }
+        }
+    }
+
+    public void SumarPunto()
+    {
+        if (juegoTerminado == false)
+        {
+            puntos++;
+
+            if (puntos >= puntosParaGanar)
+            {
+                Ganar();
+            }
+        }
+    }
+
+    void Ganar()
+    {
+        juegoTerminado = true;
+        panelWin.SetActive(true);
+        panelGameOver.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+
+    void Perder()
+    {
+        juegoTerminado = true;
+        panelGameOver.SetActive(true);
+        panelWin.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+}
